9427a46d1c3c60ab055a0aec2bf8d02a4cff5cd7
Andreas Krennmair <ak@synflood.at>
2009-02-28 23:50:25 +0100

    implemented file loading.

--- me.c
+++ me.c
@@ -2,28 +2,27 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #define clrline(i) do { move(i, 0); clrtoeol(); } while(0)
 
 typedef struct line {
-	struct line * prev; /* previous line */
-	struct line * next; /* next line */
+	struct line * prev, * next; /* previous line, next line */
 	char * text;
-	unsigned int asize; /* allocated size */
-	unsigned int usize; /* used size */
+	unsigned int asize, usize; /* allocated size, used size */
 } line_t;
 
-line_t * cur;
-unsigned int width, height;
-unsigned int x, y;
+static line_t * cur = NULL;
+static unsigned int width, height, x, y, offset;
+static char * fname = NULL;
 
-line_t * find_first(line_t * l) {
+static line_t * find_first(line_t * l) {
 	while (l->prev != NULL)
 		l = l->prev;
 	return l;
 }
 
-line_t * create_line(const char * text, unsigned int textlen) {
+static line_t * create_line(const char * text, unsigned int textlen) {
 	line_t * l = malloc(sizeof(line_t));
 	unsigned int len = text ? textlen : 0;
 	l->text = malloc(len);
@@ -32,15 +31,25 @@ line_t * create_line(const char * text, unsigned int textlen) {
 	return l;
 }
 
-void redraw_screen() {
+static inline void incr_y() {
+	if (y < height-3) y++;
+	else offset++;
+}
+
+static inline void decr_y() {
+	if (y > 0) y--;
+	else offset--;
+}
+
+static void redraw_screen() {
 	attrset(A_REVERSE);
 	clrline(height-2);
 	move(y, x);
-	mvprintw(height-2, 0, "[mein editor] [%u|%u]", y + 1, x + 1);
+	mvprintw(height-2, 0, "[mein editor] %s [%u|%u]", fname ? fname : "<no file>", offset + y + 1, x + 1);
 	attrset(A_NORMAL);
 }
 
-void draw_text() {
+static void draw_text() {
 	int i;
 	line_t * tmp = cur->prev;
 	if (y > 0) {
@@ -66,7 +75,7 @@ void draw_text() {
 	refresh();
 }
 
-void resize_line(line_t * l, size_t size) {
+static void resize_line(line_t * l, size_t size) {
 	if (l->asize < size) {
 		l->text = realloc(l->text, size);
 		l->asize = size;
@@ -74,13 +83,13 @@ void resize_line(line_t * l, size_t size) {
 	l->usize = size;
 }
 
-void insert_char(int key) {
+static void insert_char(int key) {
 	resize_line(cur, cur->usize + 1);
 	memmove(cur->text + x + 1, cur->text + x, cur->usize - x);
 	cur->text[x] = (char)key;
 }
 
-void handle_enter() {
+static void handle_enter() {
 	line_t * nl;
 	nl = create_line(cur->text + x, cur->usize - x);
 	cur->usize = x;
@@ -90,11 +99,11 @@ void handle_enter() {
 	nl->prev = cur;
 	cur->next = nl;
 	cur = cur->next;
-	y++;
+	incr_y();
 	x = 0;
 }
 
-void merge_next_line() {
+static void merge_next_line() {
 	if (cur->next) {
 		line_t * n = cur->next;
 		size_t oldsize = cur->usize;
@@ -109,7 +118,7 @@ void merge_next_line() {
 	}
 }
 
-void handle_backspace() {
+static void handle_backspace() {
 	if (x > 0) {
 		x--; 
 		memmove(cur->text + x, cur->text + x + 1, cur->usize - x - 1);
@@ -120,23 +129,59 @@ void handle_backspace() {
 			cur = cur->prev;
 			oldsize = cur->usize;
 			merge_next_line();
-			if (y > 0) y--;
+			decr_y();
 			x = oldsize;
 		}
 	}
 }
 
-int main(void) {
+static void load_file(char * filename) {
+	FILE * f = fopen(filename, "r");
+	line_t * nl;
+	char buf[1024];
+	if (!f) {
+		fprintf(stderr, "Error: couldn't open '%s'.\n", filename);
+		exit(EXIT_FAILURE);
+	}
+	cur = NULL;
+	while (!feof(f)) {
+		fgets(buf, sizeof(buf), f);
+		if (!feof(f)) {
+			size_t len = strlen(buf);
+			if (len>0 && buf[len-1] == '\n') {
+				buf[len-1] = '\0'; len--;
+			}
+			nl = create_line(buf, len);
+			if (cur) {
+				nl->next = cur->next;
+				nl->prev = cur;
+				cur->next = nl;
+				cur = cur->next;
+			} else {
+				nl->prev = nl->next = NULL;
+				cur = nl;
+			}
+		}
+	}
+	cur = find_first(cur);
+	fname = strdup(filename);
+}
+
+int main(int argc, char * argv[]) {
 	int quit_loop = 0;
 	char * kn; int key;
 
+	if (argc > 1) {
+		load_file(argv[1]);
+	} else {
+		cur = create_line("", 0);
+		cur->prev = NULL;
+		cur->next = NULL;
+	}
+
 	initscr(); cbreak(); noecho();
 	nonl(); intrflush(stdscr, FALSE); keypad(stdscr, TRUE);
-	cur = create_line("", 0);
-	cur->prev = NULL;
-	cur->next = NULL;
-
-	y = x = 0;
+	offset = y = x = 0;
 
 	getmaxyx(stdscr, height, width);
 
@@ -169,26 +214,22 @@ int main(void) {
 			case KEY_UP:
 				if (cur->prev) {
 					cur = cur->prev;
-					if (y > 0) y--;
+					decr_y();
 					if (x > cur->usize) x = cur->usize;
 				}
 				break;
 			case KEY_DOWN:
 				if (cur->next) {
 					cur = cur->next;
-					if (y < height-3) y++;
+					incr_y();
 					if (x > cur->usize) x = cur->usize;
 				}
 				break;
-			case KEY_LEFT:
-				if (x > 0) {
-					x--;
-				}
+			case KEY_LEFT: 
+				if (x > 0) x--; 
 				break;
 			case KEY_RIGHT:
-				if (x < cur->usize) {
-					x++;
-				}
+				if (x < cur->usize) x++;
 				break;
 			default:
 				if (key >= 32) {
